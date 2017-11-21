0fc6cae99902e419912c7394410724374568f08b
Aaron Weaver <aaronweaver@users.noreply.github.com>
2017-11-21 08:42:34 -0500

    Adding dependency checker

--- build-dockers.sh
+++ build-dockers.sh
@@ -3,3 +3,4 @@ docker rmi $(docker images | grep "none" | awk '/ / { print $3 }')
 echo "Building docker"
 docker build -f dockers/base/dockerfile-base . -t appsecpipeline/base
 docker build -f dockers/base/dockerfile-base-tools . -t appsecpipeline/base-tools
+docker build -f dockers/base/dockerfile-sast . -t appsecpipeline/sast
--- jenkins.bash
+++ jenkins.bash
@@ -2,3 +2,4 @@ echo
 echo "Updating Jenkins Jobs"
 jenkins-jobs --conf pipelines/jenkins/config/env/jenkins_job.ini  update -r pipelines/jenkins/templates/arachni-dojo.yaml
 jenkins-jobs --conf pipelines/jenkins/config/env/jenkins_job.ini  update -r pipelines/jenkins/templates/nikto-arachni-parallel.yaml
+jenkins-jobs --conf pipelines/jenkins/config/env/jenkins_job.ini  update -r pipelines/jenkins/templates/bodge-it.yaml
