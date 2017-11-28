861baeb7b6a12cd3d48cba7c6b991efa403250a7
Aaron Weaver <aaronweaver@users.noreply.github.com>
2017-11-28 11:05:21 -0500

    Support for Dyanmic Pipelines

--- build-dockers.sh
+++ build-dockers.sh
@@ -1,8 +1,10 @@
 echo "Keeping it tidy."
 docker rmi $(docker images | grep "none" | awk '/ / { print $3 }')
+#docker volume rm $(docker volume ls -qf dangling=true)
+#docker rmi $(docker images -q)
 echo "Building docker"
-docker build -f dockers/base/dockerfile-base . -t appsecpipeline/base
-docker build -f dockers/base/dockerfile-base-tools . -t appsecpipeline/base-tools
-docker build -f dockers/base/dockerfile-sast . -t appsecpipeline/sast
-docker build -f dockers/base/dockerfile-node . -t appsecpipeline/node
-docker build -f dockers/base/dockerfile-ruby . -t appsecpipeline/ruby
+docker build --no-cache -f dockers/base/dockerfile-base . -t appsecpipeline/base
+docker build --no-cache -f dockers/base/dockerfile-base-tools . -t appsecpipeline/base-tools
+docker build --no-cache -f dockers/base/dockerfile-sast . -t appsecpipeline/sast
+docker build --no-cache -f dockers/base/dockerfile-node . -t appsecpipeline/node
+docker build --no-cache -f dockers/base/dockerfile-ruby . -t appsecpipeline/ruby
--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,10 @@
 echo
 echo "Updating Jenkins Jobs"
+#For some reason specifying * in the directory doesn't work
 jenkins-jobs --conf pipelines/jenkins/config/env/jenkins_job.ini  update -r pipelines/jenkins/templates/arachni-dojo.yaml
 jenkins-jobs --conf pipelines/jenkins/config/env/jenkins_job.ini  update -r pipelines/jenkins/templates/nikto-arachni-parallel.yaml
 jenkins-jobs --conf pipelines/jenkins/config/env/jenkins_job.ini  update -r pipelines/jenkins/templates/bodge-it.yaml
 jenkins-jobs --conf pipelines/jenkins/config/env/jenkins_job.ini  update -r pipelines/jenkins/templates/retirejs.yaml
 jenkins-jobs --conf pipelines/jenkins/config/env/jenkins_job.ini  update -r pipelines/jenkins/templates/brakeman.yaml
 jenkins-jobs --conf pipelines/jenkins/config/env/jenkins_job.ini  update -r pipelines/jenkins/templates/synk.yaml
+jenkins-jobs --conf pipelines/jenkins/config/env/jenkins_job.ini  update -r pipelines/jenkins/templates/dynamic-pipeline.yaml
